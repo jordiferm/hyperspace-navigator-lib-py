b5d4bf546f3015680ab43be7130c577b8cc6cbd2
Jordi Fernandez <jordi.fernandez@humanitcare.com>
2022-03-20 19:14:43 +0100

    Changes the demo sample to hyperspace navigator calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 #include <pybind11/pybind11.h>
-
+#include <pybind11/stl.h>
+#include <hyperspace_navigator.hpp>
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
+//TODO, Custom type casters for Map and Navigator, see: https://pybind11.readthedocs.io/en/stable/advanced/cast/custom.html
+namespace hyperspace_navigator {
+
+std::vector<std::vector<uint64_t>> fastestRoute(std::vector<float> space) {
+    SpaceMap map = SpaceMap(space.data(), SpaceLayout({3, 3}));
+    NavigationPath navigationPath = map.fastestRoute(map.spaceStart(), map.spaceEnd());
+    return navigationPath.indexes();
+}
+
+float routeTime(std::vector<float> space, std::vector<std::vector<uint64_t>> indexes) {
+    SpaceMap map = SpaceMap(space.data(), SpaceLayout({3, 3}));
+    NavigationPath path = map.navigationPath(indexes);
+    return map.time(path);
+}
+
 }
 
 namespace py = pybind11;
@@ -23,17 +37,16 @@ PYBIND11_MODULE(cmake_example, m) {
            subtract
     )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
+    m.def("fastestRoute", &hyperspace_navigator::fastestRoute, R"pbdoc(
+        Calculates the fastest route given a space and its layout
+        returns a navigationPath
 
-        Some other explanation about the add function.
     )pbdoc");
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
+    m.def("routeTime", &hyperspace_navigator::routeTime, R"pbdoc(
+        Given a navigationPath and a space it returns the time to navigate through this path in the space
     )pbdoc");
+
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,27 @@
 import cmake_example as m
+import pytest
 
 
-def test_main():
+@pytest.fixture
+def space():
+    return [0, 1, 3,
+            5, 2, 8,
+            1, 5, 6]
+
+
+@pytest.fixture
+def fastest_route():
+    return [[0, 0], [1, 0], [1, 1], [1, 2], [2, 2]]
+
+
+def test_2d_3x3_space(space, fastest_route):
+    navigation_path = m.fastestRoute(space)
     assert m.__version__ == "0.0.1"
-    assert m.add(1, 2) == 3
-    assert m.subtract(1, 2) == -1
+    assert len(navigation_path) == 5
+    assert navigation_path == fastest_route
+
+
+def test_get_navigation_time(space, fastest_route):
+    navigation_time = m.routeTime(space, fastest_route)
+    assert navigation_time is not None
+    assert navigation_time == 14
